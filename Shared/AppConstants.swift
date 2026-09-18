86d84af2a4d19af10e7fc9fd8e5052cb482c8a4c
shaya99stern-hash <shaya99stern@gmail.com>
2026-09-18 18:33:59 -0400

    Add Shared/AppConstants.swift

---
+++ Shared/AppConstants.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum AppConstants {
+    static let appGroup = "group.com.navios.widgets"
+    static let tasksKey = "navios.tasks.v1"
+    static let lastLatitudeKey = "navios.location.latitude"
+    static let lastLongitudeKey = "navios.location.longitude"
+    static let lastLocationDateKey = "navios.location.date"
+}
