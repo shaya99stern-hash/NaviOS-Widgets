4454885ce7ec11b045d27ad1d4eacff16bd2afb1
shaya99stern-hash <shaya99stern@gmail.com>
2026-09-18 18:40:45 -0400

    Fix Swift 6 concurrency metadata in NaviOSWidget/WidgetListIntent.swift

--- NaviOSWidget/WidgetListIntent.swift
+++ NaviOSWidget/WidgetListIntent.swift
@@ -2,8 +2,10 @@ import AppIntents
 import WidgetKit
 
 struct WidgetListIntent: WidgetConfigurationIntent {
-    static var title: LocalizedStringResource = "NaviOS List"
-    static var description = IntentDescription("Choose which NaviOS list this widget shows.")
+    nonisolated static var title: LocalizedStringResource { "NaviOS List" }
+    nonisolated static var description: IntentDescription {
+        IntentDescription("Choose which NaviOS list this widget shows.")
+    }
 
     @Parameter(title: "List", default: .personal)
     var list: TaskListKind
