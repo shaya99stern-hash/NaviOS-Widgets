ac824ce21e23362649e1ecb331cd6081309ed4b7
shaya99stern-hash <shaya99stern@gmail.com>
2026-09-19 00:08:00 -0400

    Complete Studio web source for Vercel

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE = 'navios-widget-studio-v1';
+const ASSETS = ['./', './manifest.webmanifest'];
+self.addEventListener('install', e =>
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)))
+);
+self.addEventListener('fetch', e =>
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
+);
